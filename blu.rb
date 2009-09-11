74777e9539cf24804a7b4a807a1dd2895a9c53a4
Greg Borenstein <greg@mfdz.com>
2009-09-11 15:49:15 -0400

    class file, test file, example dir

---
+++ blu.rb
@@ -0,0 +1,4 @@
+class Blu
+  def initialize(path_to_blu)
+  end
+end
---
+++ blu_test.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+
+class LoadBlu < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_load_error_blu
+  end
+end
