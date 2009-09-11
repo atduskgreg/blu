705dcb06857256554293afce35e92b4fa8dc5542
Greg Borenstein <greg@mfdz.com>
2009-09-11 16:02:46 -0400

    basic parsing

--- blu.rb
+++ blu.rb
@@ -1,4 +1,23 @@
 class Blu
+  SOUND_TYPES = %w{square}
+  
+  attr_accessor :settings
+  
+  
   def initialize(path_to_blu)
+    @raw = File.open(path_to_blu).read
+  end
+  
+  def parse!
+    result = {}
+    @raw.split(/\n/).each do |line| 
+      words = line.split(" ");
+      if ( words.first.downcase == "type")
+        result[:type] = words.last.to_sym
+      else
+        result[words.first.to_sym] = words.last.to_f
+      end
+    end
+    @settings = result
   end
 end
--- blu_test.rb
+++ blu_test.rb
@@ -1,12 +1,28 @@
 require 'test/unit'
+require 'blu'
 
 class LoadBlu < Test::Unit::TestCase
-  # def setup
-  # end
+  def setup
+    @b = Blu.new File.expand_path(File.dirname(__FILE__)) + "/blus/error.blu"
+  end
 
   # def teardown
   # end
 
-  def test_load_error_blu
+  def test_should_find_blu_file
+    assert_equal( "type    square\nsustain 0.266\ndecay   0.187\nfreq    0.268\nslide   0.179\nsquare  0.326\nvibe    0.227\nvspeed  0.231", @b.instance_variable_get("@raw"))
+  end
+  
+  def test_parse_error_blue
+    @b.parse!
+    assert_equal(
+    {:type   => :square,
+    :sustain => 0.266,
+    :decay   => 0.187,
+    :freq   => 0.268,
+    :slide =>  0.179,
+    :square =>  0.326,
+    :vibe  =>  0.227,
+    :vspeed =>  0.231}, @b.settings)
   end
 end
